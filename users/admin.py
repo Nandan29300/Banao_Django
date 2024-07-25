6f707972671b91b43fadc9a8eb404e6b68b51bb3
Nandan P Aghera <agheranandanp@gmail.com>
2024-07-25 21:13:24 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,12 @@
-
-
-# Register your models here.
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, BlogPost
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'user_type')
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'is_draft')
+    list_filter = ('category', 'is_draft')
+    search_fields = ('title', 'summary', 'content')
