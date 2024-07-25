72027b857f7f3195118ef7d136f598444b9f1a5d
Nandan P Aghera <agheranandanp@gmail.com>
2024-07-25 21:12:41 +0530

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import CustomUser, BlogPost
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'first_name', 'last_name', 'profile_picture', 'user_type', 'address_line1', 'city', 'state', 'pincode']
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'image', 'category', 'summary', 'content', 'is_draft']
