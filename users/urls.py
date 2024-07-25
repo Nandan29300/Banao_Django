3707f4283a9ee283abe7be87983f004a5fc7023a
Nandan P Aghera <agheranandanp@gmail.com>
2024-07-25 21:11:49 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('create_blog/', views.create_blog, name='create_blog'),
+    path('view_blogs/', views.view_blogs, name='view_blogs'),
+    path('logout/', views.logout_user, name='logout'),
+    path('blog/delete/<int:blog_id>/', views.delete_blog, name='delete_blog'),
 ]
 
 
