cf880317f27ffa69049094580b4208707030617e
Nandan P Aghera <agheranandanp@gmail.com>
2024-07-25 21:10:19 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout
+from django.shortcuts import get_object_or_404, redirect
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, BlogPostForm
+from .models import BlogPost
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method == 'POST':
@@ -35,7 +39,46 @@ def login_view(request):
     return render(request, 'login.html', {'form': form})
 
 def dashboard(request):
-    # Ensure that the user is logged in before rendering the dashboard
     if not request.user.is_authenticated:
-        return redirect('login')  # Ensure URL name matches
+        return redirect('login')
+    
     return render(request, 'dashboard.html')
+
+def create_blog(request):
+    if request.user.user_type != 'doctor':
+        return redirect('dashboard')
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog_post = form.save(commit=False)
+            blog_post.author = request.user
+            blog_post.save()
+            return redirect('dashboard')
+    else:
+        form = BlogPostForm()
+    return render(request, 'create_blog.html', {'form': form})
+@login_required
+def view_blogs(request):
+    if request.user.user_type == 'doctor':
+        blogs = BlogPost.objects.filter(author=request.user)
+    else:
+        blogs = BlogPost.objects.filter(is_draft=False)
+    return render(request, 'view_blogs.html', {'blogs': blogs})
+
+@login_required
+def delete_blog(request, blog_id):
+    blog = get_object_or_404(BlogPost, id=blog_id)
+
+    # Check if the user is the author of the blog and is a doctor
+    if blog.author == request.user and request.user.user_type == 'doctor':
+        if request.method == 'POST':
+            blog.delete()
+            return redirect('view_blogs')  # Redirect to the list of blogs
+    else:
+        raise PermissionDenied
+    
+    return render(request, 'confirm_delete.html', {'blog': blog})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
